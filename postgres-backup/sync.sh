b511f194c438c58fefeb0ce52216e73658d25f69
Alexander Czigler <alexander@iteam.se>
2020-06-08 11:19:50 +0200

    fix: conditional sync to s3 or sftp

--- postgres-backup/sync.sh
+++ postgres-backup/sync.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Sync backups to S3"
-s3cmd sync /backup s3://${S3_BUCKET}/${S3_DIR}/ --skip-existing
+if [ ! -z "${S3_BUCKET}" ]; then
+  echo "Sync archives to S3"
+  s3cmd -v sync /backup s3://${S3_BUCKET}/${S3_DIR}/ --skip-existing
+fi
 
-echo "Sync backups to sftp"
-cd /backup
-find . -name '*.psql.gz' -exec curl -v --insecure -T '{}' sftp://${SSH_USER}:${SSH_PASS}@${SSH_HOST}/${SSH_PATH}/'{}' \;
+if [ ! -z "${SSH_HOST}" ]; then
+  echo "Sync archives to sftp"
+  cd /backup
+  find . -name '*.psql.gz' -exec curl -v --insecure -T '{}' sftp://${SSH_USER}:${SSH_PASS}@${SSH_HOST}/${SSH_PATH}/'{}' \;
+fi
