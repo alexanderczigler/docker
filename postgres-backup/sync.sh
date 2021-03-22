9361af282db828248a0bdb36488d02269f6cd6c0
Adam Näslund <adam.naslund@iteam.se>
2021-03-22 20:57:20 +0100

    fix: add escaping to handle strings and files with spaces

--- postgres-backup/sync.sh
+++ postgres-backup/sync.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -z "${S3_BUCKET}" ]; then
+if [ -n "${S3_BUCKET}" ]; then
   echo "Sync archives to S3"
-  s3cmd -v sync /backup s3://${S3_BUCKET}/${S3_DIR}/ --skip-existing
+  s3cmd -v sync /backup "s3://${S3_BUCKET}/${S3_DIR}/" --skip-existing
 fi
 
-if [ ! -z "${SSH_HOST}" ]; then
+if [ -n "${SSH_HOST}" ]; then
   echo "Sync archives to sftp"
   cd /backup
-  find . -name '*.psql.gz' -exec curl -v --insecure -T '{}' sftp://${SSH_USER}:${SSH_PASS}@${SSH_HOST}/${SSH_PATH}/'{}' \;
+  find . -name '*.psql.gz' -exec curl -v --insecure -T '{}' "sftp://${SSH_USER}:${SSH_PASS}@${SSH_HOST}/${SSH_PATH}/"'{}' \;
 fi
